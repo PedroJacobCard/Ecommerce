3f1692f982bd45ed34a12511602b974a071fd0f7
Pedro Henrique Jacob Cardoso <pedrojacob05cardoso@gmail.com>
2023-03-17 10:42:18 -0300

    ja

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ if (close) {
 /* searcher products */
 
 const searchItems = document.querySelector("#search-items");
-const searchBox = document.querySelector(".search-box");
+const searchBox = document.querySelectorAll(".search-box");
 const products = document.querySelectorAll(".pro");
 
 window.addEventListener("load", () => {
@@ -48,7 +48,7 @@ searchItems.addEventListener("keyup", () => {
     let filter = searchItems.value.toLocaleUpperCase();
     let a = searchBox.getElementsByTagName("a");
     for (let i = 0; i < a.length; i++){
-        let b = a[i].querySelector(".product-details");
+        let b = a[i].querySelectorAll(".product-details");
         let c = b.querySelector("h3");
 
         let textValue = c.textContent || c.innerText;
